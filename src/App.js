799bde300dcd911779c31d8b871b1da4c147ab50
sandeep.goswami.in <sandeep.goswami.in@rupeekits-MacBook-Pro.local>
2024-01-21 17:43:12 +0530

    sign up and sign in with glassmorphism

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Registration from "./Components/Registration";
 import Login from "./Components/Login";
 import MainPage from "./Components/MainPage";
 import UserProfile from "./Components/UserProfile";
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import EventDetails from "./Components/EventDetails";
 // import {Typography} from '@material-ui/core';
 function App() {
@@ -10,10 +10,10 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Registration />} />
-        <Route path="/login" element = {<Login/>} />
-        <Route path ="/home" element = {<MainPage />} />
-        <Route path ="/user" element = {<UserProfile />} />
-        <Route path ="/event-details" element = {<EventDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<MainPage />} />
+        <Route path="/user" element={<UserProfile />} />
+        <Route path="/event-details" element={<EventDetails />} />
       </Routes>
     </>
   );
