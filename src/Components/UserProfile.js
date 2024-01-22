936ad2df0ca72aad46f13ebc140d32a3bc57e0db
vishvas04 <vishvaskalyan04@gmail.com>
2024-01-22 20:32:05 +0530

    user profile changed

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -6,27 +6,13 @@ import {
   Avatar,
   Select,
   MenuItem,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemSecondaryAction,
-  IconButton,
+  Chip,
   Divider,
   Grid,
 } from '@mui/material';
 import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
+// import DeleteIcon from '@mui/icons-material/Delete';
 import './UserProfile.css';
-
-const interestImages = {
-  Dancing: 'https://rukminim2.flixcart.com/image/400/480/ky4qgsw0/sticker/l/v/r/medium-dance-wall-decals-wall-stickers-for-dance-girl-dancing-original-imagafg5xehnq4jz.jpeg?q=20&crop=false',
-  Singing: 'https://classroomclipart.com/image/static7/preview2/young-girl-standing-on-stage-holding-microphone-singing-surround-58786.jpg',
-  'Space Exploration': 'https://media.istockphoto.com/id/1353874144/photo/astronaut-in-outer-space-spaceman-with-starry-and-galactic-background-sci-fi-digital-wallpaper.jpg?s=300',
-  'Writing Blogs': 'https://www.aprompt.co.uk/uploads/blog/bloggers-wiltshire.jpge',
-  Sports: 'https://media.npr.org/assets/img/2020/06/10/gettyimages-200199027-001-b5fb3d8d8469ab744d9e97706fa67bc5c0e4fa40.jpg',
-  // Add more interests and their corresponding images
-};
-
 const eventImages = {
   'Event 1': 'URL_TO_EVENT_IMAGE_1',
   'Event 2': 'URL_TO_EVENT_IMAGE_2',
@@ -37,7 +23,7 @@ const eventImages = {
 const UserProfile = () => {
   const [selectedInterest, setSelectedInterest] = useState('');
   const [interests, setInterests] = useState([]);
-  const [events, setEvents] = useState(['Event 1', 'Event 2', 'Event 3', 'Event 4', 'Event 5', 'Event 6', 'Event 7', 'Event 8','Event 9']); // Placeholder for registered events
+  const [events, setEvents] = useState(['vishvas']); // Placeholder for registered events
 
   const handleAddInterest = () => {
     if (selectedInterest.trim() !== '') {
@@ -54,13 +40,6 @@ const UserProfile = () => {
     newInterests.splice(index, 1);
     setInterests(newInterests);
   };
-
-  const handleDeleteEvent = (index) => {
-    const newEvents = [...events];
-    newEvents.splice(index, 1);
-    setEvents(newEvents);
-  };
-
   return (
     <Grid container spacing={3} justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
       <Grid item xs={12} md={6}>
@@ -91,11 +70,12 @@ const UserProfile = () => {
             </Typography>
             <Select
               className="select1"
+              // style={{width:'400px'}}
               value={selectedInterest}
               onChange={(e) => setSelectedInterest(e.target.value)}
               variant="outlined"
               margin="normal"
-              sx={{ background: '#ffffff', flex: '75%', marginRight: '5%' }}
+              sx={{ background: '#ffffff', flex: '12%', marginRight: '5%' }}
             >
               <MenuItem value="Dancing">Dancing</MenuItem>
               <MenuItem value="Singing">Singing</MenuItem>
@@ -109,65 +89,59 @@ const UserProfile = () => {
           <Divider style={{ margin: '20px 0', background: '#ffffff' }} />
 
           <div>
-            <Typography variant="h6" gutterBottom>
+            <Typography variant="h6">
               Registered Interests
             </Typography>
           </div>
           <div className='box1' style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }}>
             {interests.map((interest, index) => (
-              <Paper key={index} style={{ width: '150px', margin: '10px', padding: '10px', borderRadius: '15px', position: 'relative', boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)' }}>
-                <div style={{ backgroundImage: `url(${interestImages[interest]})`, backgroundSize: 'cover', height: '80px', borderRadius: '10px', marginBottom: '10px' }} />
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                  <Typography variant="body1" style={{ textAlign: 'center', fontWeight: 'bold', color: 'red', flex: 1 }}>{interest}</Typography>
-                  <IconButton
-                    edge="end"
-                    aria-label="delete"
-                    onClick={() => handleDeleteInterest(index)}
-                    style={{ color: '#000000' }}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </div>
-              </Paper>
+              <Chip
+                key={index}
+                label={interest}
+                onDelete={() => handleDeleteInterest(index)}
+                color="primary" // Change color here
+                size="medium" // Change size here
+                style={{ margin: '5px' }}
+              />
             ))}
           </div>
         </Paper>
       </Grid>
 
-      <Grid item xs={12} md={6}>
-        <Paper
-          elevation={10}
-          style={{
-            padding: '20px',
-            background: 'linear-gradient(45deg, #4d86f7 30%, #2a5bb1 90%)',
-            color: '#ffffff',
-            borderRadius: '20px',
-            marginTop: '20px',
-          }}
-        >
-          <Typography variant="h6" gutterBottom>
-            Registered Events
-          </Typography>
-          <div className='box1' style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-            {events.map((event, index) => (
-              <Paper key={index} style={{ width: '150px', margin: '10px', padding: '10px', borderRadius: '15px', position: 'relative', boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)' }}>
-                <div style={{ backgroundImage: `url(${eventImages[event]})`, backgroundSize: 'cover', height: '80px', borderRadius: '10px', marginBottom: '10px' }} />
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center',justifyContent:'space-evenlyx' }}>
-                  <Typography variant="body1" style={{ textAlign: 'center', fontWeight: 'bold', color: 'red', flex: 1 }}>{event}</Typography>
-                  <IconButton
-                    edge="end"
-                    aria-label="delete"
-                    onClick={() => handleDeleteEvent(index)}
-                    style={{ color: '#000000', marginTop: '5px' }}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </div>
-              </Paper>
-            ))}
-          </div>
-        </Paper>
-      </Grid>
+      {events.length > 0 && (
+        <Grid item xs={12} md={6} justifyContent="center" alignItems="center" style={{ height: '70vh' }}>
+          <Paper
+            elevation={10}
+            style={{
+              padding: '20px',
+              background: '',
+              color: '#ffffff',
+              borderRadius: '20px',
+              marginTop: '20px',
+            }}
+          >
+            <Typography variant="h6" gutterBottom style={{color:'red'}}>
+              Registered Events
+            </Typography>
+            <div
+              className='box1'
+              style={{
+                overflowY: 'auto',
+                maxHeight: '399px', // Set the maximum height here
+              }}
+            >
+              {events.map((event, index) => (
+                <Paper key={index} style={{ margin: '10px', padding: '10px', borderRadius: '15px', position: 'relative', boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)', height: '359px' }}>
+                  <div style={{ backgroundImage: `url(${eventImages[event]})`, backgroundSize: 'cover', height: '200px', borderRadius: '10px', marginBottom: '10px' }} />
+                  <div style={{ textAlign: 'center' }}>
+                    <Typography variant="body1" style={{ fontWeight: 'bold', color: 'red' }}>{event}</Typography>
+                  </div>
+                </Paper>
+              ))}
+            </div>
+          </Paper>
+        </Grid>
+      )}
     </Grid>
   );
 };
